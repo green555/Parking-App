517228f016f9ef659adf4a5b37943ba72c2a2c59
Wei Wei <weiweibcm@gmail.com>
2022-07-06 14:45:56 -0700

    flask run ssl_context added

--- server.py
+++ server.py
@@ -262,4 +262,5 @@ def clear_session():
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True, port=5002)
+    context = ('local.crt', 'local.key')
+    app.run(host="0.0.0.0", debug=True, port=5002, ssl_context=context)
